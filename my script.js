c61e5c5552e6c89dffc7c979b01007e7482d4546
Mahitab24 <143906104+Mahitab24@users.noreply.github.com>
2023-09-16 07:15:51 -0700

    Add files via upload

---
+++ my script.js
@@ -0,0 +1,48 @@
+//const todos=[
+	//{
+		//id:1,
+		//text: 'Take out trash',
+		//isCompleted:true
+	//},
+	//{
+		//id:2,
+		//text: 'Meeting the boss',
+		//isCompleted:true
+	//},{
+		//id:3,
+		//text: 'Dentist meeting',
+		//isCompleted:false
+	//}
+//];
+//console.log(todos);
+
+// forEach, map, filter
+
+
+//const todoText = todos.map(function(todo){
+	//return todo.text;
+//});
+//console.log(todo.Text);
+
+
+//for(let i = 0; i < 10; i++){
+	//console.log('inloop:',i);
+//}
+//console.log('loop finished');
+
+
+const para = document.querySelector('h1');
+//console.log(para.innerText);
+//para.innerText = 'New text';
+
+const paras = document.querySelectorAll('h1');
+//paras.forEach(para => {
+	//console.log(para.innerText);
+	//para.innerText += 'island of art';
+	//});
+
+const content = document.querySelector('.content');
+console.log(content.innerHTML);
+content.innerHTML='<h3>ISLAND OF ART</h3>';
+
+
---
+++ my script1.js
@@ -0,0 +1,23 @@
+// loop
+//for(let i = 0; i < 10; i++){
+	//console.log('inloop:', i);
+//}
+//console.log('loop finished');
+
+//const name =['mostafa loves history','mostafa loves history','mostafa loves history','mostafa loves history'];
+
+//for(let i=0; i<name.length;i++){
+	//console.log(name[i]);
+//}
+//console.log('loop finished');
+
+const para = document.querySelector('h1');
+//para.setAttribute('style', 'margine : 50px;');
+
+
+console.log(para.style); 
+console.log(para.style.color);
+
+para.style.color='#CDAF53';
+para.style.fontSize='100px';
+
